ff4b0e39b6c7e77aa39be0e69914f1caff436245
lero1312 <ma13121999@gmail.com>
2019-04-29 21:03:50 +0200

    graph

---
+++ test.py
@@ -0,0 +1,78 @@
+from tkinter import *
+import matplotlib.pyplot as plt
+
+
+def showHist(x, y):
+    '''
+    :param x: list of integers OX
+    :param y: list of integers OY
+    :return: none
+    '''
+    x = list(map(float,x))
+    y = list(map(float,y))
+    x.sort()
+    y.sort()
+    plt.hist(x,y,rwidth=0.9)
+    plt.show()
+
+
+def showBoxPlot(x):
+    '''
+    :param x: list of integers OX
+    :return: none
+    '''
+    x = list(map(float, x))
+    x.sort()
+    plt.boxplot(x)
+    plt.show()
+
+
+def showScatterPlot(x, y):
+    '''
+    :param x: list of integers OX
+    :param y: list of integers OY
+    :return: none
+    '''
+    x = list(map(float, x))
+    y = list(map(float, y))
+    x.sort()
+    y.sort()
+    plt.boxplot(x, y)
+    plt.show()
+
+
+def showBarPlot(x):
+    '''
+    :param x: list of integers OX
+    :return: none
+    '''
+    x = list(map(float, x))
+    x.sort()
+    plt.bar(x)
+    plt.show()
+
+
+def showPiePlot(x):
+    '''
+    :param x: list of integers OX
+    :return: none
+    '''
+    x = list(map(float, x))
+    x.sort()
+    plt.pie(x)
+    plt.show()
+
+
+def showPlot(x, y):
+    '''
+    :param x: list of integers OX
+    :param y: list of integers OY
+    :return: none
+    '''
+    x = list(map(float, x))
+    y = list(map(float, y))
+    x.sort()
+    y.sort()
+    plt.plot(x, y)
+    plt.show()
+
