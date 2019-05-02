18e6059c1bdc6448a4f88cffb8e5a511cbe2c5d1
Bohoty <omarbohoty@gmail.com>
2019-05-02 21:54:52 +0200

    Bohoty2

--- number.py
+++ number.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 import math
 matplotlib.style.use('ggplot')
 
+def cMed(val):
+    if (len(val)%2 != 0):
+        return val[int(len(val) / 2)]
+    else:
+        return (val[int(len(val) / 2)] + val[int(len(val) / 2) - 1]) / 2
 
-def cNoIntervals(values):
-    z = int(math.log(len(values), 2) + 2)
-    return z
+def cLargstOutlier(m, val):
+    q3 = Q3(m, int(m / 2), val)
+    iqr = cIQR(m, val)
+    return q3 + 1.5*iqr
 
 
 
@@ -19,89 +25,80 @@ def cLenInterval(val, z):
 
 
 def cMean(values):
-    mean = 0
+    Mean = 0
     for i in values:
-        mean += i
-    mean /= len(values)
-    return mean
+        Mean += i
+    Mean /= len(values)
+    return Mean
 
 
-values = [int(i) for i in input().split()]
-numOfData = len(values)
-values.sort()
+val = [int(i) for i in input().split()]
+numOfData = len(val)
+val.sort()
 
-#Calculate IQR
-def calcIQR(n , values):
-    return calcQ3(n , int(n/2) , values) - calcQ1(int(n/2)-1 , values)
+def cIQR(m, val):
+    return Q3(m, int(m / 2), val) - Q1(int(m / 2) - 1, val)
 
 
-def calcMedian(values):
-    if (len(values)%2 != 0):
-        return values[int(len(values)/2)]
-    else:
-        return (values[int(len(values)/2)] + values[int(len(values)/2)-1])/2
+def cNoIntervals(values):
+    z = int(math.log(len(values), 2) + 2)
+    return z
 
 
 
-def calcMode(val):
-    freq = {}
-    mx = 0
-    cnt = 0
+
+def cMode(val):
+    C = 0
+    MAX = 0
     mode = 0
+    F = {}
     for i in val:
-        if (i in freq):
-            freq[i] += 1
+        if (i in F):
+            F[i] += 1
         else:
-            freq[i]=1
-        mx = max(mx , freq[i])
+            F[i]=1
+        MAX = max(MAX , F[i])
 
-    for key , val in freq.items():
-        if val==mx:
-            cnt += 1
+    for key , val in F.items():
+        if val==MAX:
+            C += 1
             mode = key
 
-    if cnt > 1:
+    if C > 1:
         return  -1
     return  mode
 
 
-#Calculate Smalest Out liers
-def calcSmallestOutliers(n , values):
-    q1 = calcQ1(int(n/2)-1 , values)
-    IQR = calcIQR(n , values)
-    return q1 - 1.5*IQR
 
-def calcQ2(n , values):
-    if (n%2 != 0):
-        return values[int(n/2)]
+
+def Q2(m, val):
+    if (m%2 != 0):
+        return val[int(m / 2)]
     else:
-        return (values[int(n/2)] + values[int(n/2)-1])/2
+        return (val[int(m / 2)] + val[int(m / 2) - 1]) / 2
 
 
 
-def calcQ1(n , values):
-    if (n%2 == 0):
-        return values[int(n/2)]
+def Q1(m, val):
+    if (m%2 == 0):
+        return val[int(m / 2)]
     else:
-        return  (values[int(n/2)] + values[int(n/2)+1])/2
+        return (val[int(m / 2)] + val[int(m / 2) + 1]) / 2
 
 
-def calcQ3(n , n2 , values):
-    if (n%2 != 0):
-        n2 += 1
+def Q3(m, m2, val):
+    if (m%2 != 0):
+        m2 += 1
 
-    if ((n-n2)%2 != 0):
-        return  values[int((n+n2)/2)]
+    if ((m - m2)%2 != 0):
+        return  val[int((m + m2) / 2)]
     else:
-        return (values[int((n+n2-1)/2)] + values[int((n+n2-1)/2)+1])/2
+        return (val[int((m + m2 - 1) / 2)] + val[int((m + m2 - 1) / 2) + 1]) / 2
 
 
 
-#Calculate Largest Out liers
-def calcLargestOutliers(n , values):
-    q3 = calcQ3(n , int(n/2) , values)
-    IQR = calcIQR(n, values)
-    return q3 + 1.5*IQR
 
-
-
+def cSmlstOutlier(m, val):
+    q1 = Q1(int(m / 2) - 1, val)
+    iqr = cIQR(m, val)
+    return q1 - 1.5*iqr
